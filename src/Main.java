d3d6b2a466ebdb2fc2c0e99feb7b2b6968eb86db
BT_1E10_20 <sample@bths.edu>
2022-03-01 12:09:43 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        /*String[][] s = {{"Abby","Don","George","Kim"},{"Brian","Elenor","Harry","Lenny"},{"Cathy","Fred","Jill","Matt"}};
+        s[1][2]="Paul";
+        String t = s[2][3];
+        s[2][3]=s[0][0];
+        s[0][0]=t;
+        String[] temp = s[0];
+        s[0]=s[1];
+        s[1]=temp;
+        for (String[] st : s){
+            System.out.println(Arrays.toString(st));
+        }
+        System.out.println(s[0][2] + s[2][0]);
+        int[][] arr1=new int[2][3];
+        int[][] arr2=new int[3][2];
+        arr1[0][0]=1;
+        arr1[0][1]=2;
+        arr1[0][2]=3;
+        arr1[1][0]=4;
+        arr1[1][1]=5;
+        arr1[1][2]=6;
+        arr2[0][0]=1;
+        arr2[0][1]=4;
+        arr2[1][0]=2;
+        arr2[1][1]=5;
+        arr2[2][0]=3;
+        arr2[2][1]=6;
+        for (int[] i : arr1){
+            System.out.println(Arrays.toString(i));
+        }
+        for (int[] i : arr2){
+            System.out.println(Arrays.toString(i));
+        }*/
+
+    }
+}
